e1cafa4a0684d56468f5b62de8ad7dfa45236ea3
HarshankPatel9038 <patelharshank8@gmail.com>
2023-04-05 22:19:03 +0530

    String(05-04-2023)

---
+++ String/functions.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+
+int main() {
+	
+	char str[100], str1[100];
+	int n;
+	
+	printf("enter a string: ");
+	gets(str);
+
+
+	
+//	n = strlen(str);
+//	printf("%d", n);
+
+
+
+
+//    strcpy(str1, str);
+//    puts(str1);
+
+
+
+
+//	printf("enter a string: ");
+//	gets(str1);
+//	
+//	strcat(str, str1);
+//	puts(str);
+
+
+
+
+//	printf("enter a string: ");
+//	gets(str1);
+//	
+//	if (strcmp(str, str1) == 0) {
+//		printf("string are same");
+//	} else {
+//		printf("string are not same");
+//	}
+
+
+
+
+
+//    strrev(str);
+//    puts(str);
+
+
+
+
+
+//    strlwr(str);
+//    puts(str);
+
+
+
+
+//    strupr(str);
+//    puts(str);
+
+
+
+	return 0;
+	
+}
---
+++ String/remove-space.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+	
+	int i, j;
+	char str[100];
+	
+	printf("enter a string: ");
+	gets(str);
+	
+	i = 0;
+	while (str[i] != '\0') {
+		if (str[i] == ' ') {
+			for (j=i; str[j] != '\0'; j++) {
+				str[j] = str[j + 1];
+			}
+		}
+		i++;
+	}
+	
+	puts(str);
+	
+	return 0;
+	
+}
